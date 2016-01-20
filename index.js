9273b0daf6a8605a2cca89cc9f37a37c7225bda0
akameco <akameco.t@gmail.com>
2016-01-21 03:41:59 +0900

    Fix format

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 'use strict';
-const csv = require('csv');
 const fs = require('fs');
-
-let file = process.argv[2];
-let base = 1200;
+const base = 1200;
 
 function calc(file) {
   fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
     const lines = data.split('\n');
-    const header = lines.shift();
+    lines.shift();
+
     const ans = lines
       .map(x => x.split(','))
       .filter(x => x[1] !== '' && x[1] !== undefined)
       .map(x => {
         const start = new Date(x[2]);
         const end = new Date(x[3]);
-        let t = x[4].split(':').map(x => Number(x));
+        const t = x[4].split(':').map(x => Number(x));
         const breakTime = t[0] + t[1] / 60;
         return ((end - start) / 1000 / 60 / 60) - breakTime;
       })
-      .reduce((sum,x) => sum + x, 0);
-    console.log(ans * 1200);
+      .reduce((sum, x) => sum + x, 0);
+    console.log(ans * base);
   });
 }
 

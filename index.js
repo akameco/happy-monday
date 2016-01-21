7fcb9a9e559cb6d5fcc9dff2611dc82f61b63d70
akameco <akameco.t@gmail.com>
2016-01-22 01:53:57 +0900

    Update

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 'use strict';
 const fs = require('fs');
-const base = 1200;
 
-function calc(file) {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.log(err);
-    }
+module.exports = (file, salary, carfare) => {
+  salary = salary || 0;
+  carfare = carfare || 0;
+  return new Promise(resolve => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        console.log('require csv file');
+        process.exit(1);
+      }
 
-    const lines = data.split('\n');
-    lines.shift();
+      const lines = data.split('\n');
+      lines.shift();
 
-    const ans = lines
-      .map(x => x.split(','))
-      .filter(x => x[1] !== '' && x[1] !== undefined)
-      .map(x => {
-        const start = new Date(x[2]);
-        const end = new Date(x[3]);
-        const t = x[4].split(':').map(x => Number(x));
-        const breakTime = t[0] + t[1] / 60;
-        return ((end - start) / 1000 / 60 / 60) - breakTime;
-      })
-      .reduce((sum, x) => sum + x, 0);
-    console.log(ans * base);
+      const works = lines
+        .map(x => x.split(','))
+        .filter(x => x[1] !== '' && x[1])
+        .map(x => {
+          const start = new Date(x[2]);
+          const end = new Date(x[3]);
+          const t = x[4].split(':').map(x => Number(x));
+          const breakTime = t[0] + t[1] / 60;
+          return ((end - start) / 1000 / 60 / 60) - breakTime;
+        });
+
+      const salarySum = works.reduce((sum, x) => sum + x, 0) * salary;
+      const carfareSum = works.length * carfare * 2;
+      resolve(salarySum + carfareSum);
+    });
   });
-}
-
-module.exports = calc;
+};
